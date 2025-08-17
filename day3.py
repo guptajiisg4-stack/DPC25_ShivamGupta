7ee3e5d4e147e446a5e061cd5950d7c9b7a30cac
guptajiisg4-stack <guptajii.sg4@gmail.com>
2025-08-17 10:57:28 +0530

    Create day3.py
    Day 3 Challenge

---
+++ day3.py
@@ -0,0 +1,19 @@
+def findDuplicate(arr):
+    slow = arr[0]
+    fast = arr[0]
+
+    while True:
+        slow = arr[slow]
+        fast = arr[arr[fast]]
+        if slow == fast:
+            break
+
+    slow = arr[0]
+    while slow != fast:
+        slow = arr[slow]
+        fast = arr[fast]
+
+    return slow
+
+arr = list(map(int, input("Enter elements separated by space: ").split()))
+print("Duplicate number:", findDuplicate(arr))
