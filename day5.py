4f5ca6f3bca6c40f506b1312727deac657f30b42
guptajiisg4-stack <guptajii.sg4@gmail.com>
2025-08-19 12:33:37 +0530

    Create day5.py
    Day 5 Challenge

---
+++ day5.py
@@ -0,0 +1,15 @@
+def findLeaders(arr):
+    n = len(arr)
+    leaders = []
+    max_from_right = float('-inf')
+    
+    for i in range(n-1, -1, -1):
+        if arr[i] >= max_from_right:
+            leaders.append(arr[i])
+            max_from_right = arr[i]
+    
+    leaders.reverse()
+    return leaders
+
+arr = [16, 17, 4, 3, 5, 2]
+print("Leaders:", findLeaders(arr))
